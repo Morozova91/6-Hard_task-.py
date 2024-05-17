5f3852e54af8a3a58e24cf45b50b210c5a558d05
Юлия <118264321+Morozova91@users.noreply.github.com>
2024-05-17 08:01:22 +0300

    Add files via upload

---
+++ hard_task.py
@@ -0,0 +1,12 @@
+name = 'Черешня'
+weight = 4.5
+price = 34
+count = 4.5 * 34
+sum = 500
+change = sum - count
+print(f' Чек: Название товара: {name}')
+print(f' Вес: {weight} кг')
+print(f' Цена за 1 кг: {price} руб/кг')
+print(f' Итого: {count} руб ')
+print(f' Внесено: {sum} руб')
+print(f' Сдача: {change} руб')
